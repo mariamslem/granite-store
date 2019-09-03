fad9d82d218481b4398531bf26bad70fede25adc
mariamslem <20161922@fue.edu.eg>
2019-09-03 15:40:51 +0200

    followed up

---
+++ GraniteOnlineStore/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GraniteOnlineStore;
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using GraniteOnlineStore.Data;
+using GraniteOnlineStore.Models;
+
+namespace GraniteOnlineStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    
+    public class ProductTypesController : Controller
+    {
+        private readonly Data.ApplicationDbContext _db;
+
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(ProductTypes productTypes)
+        {
+
+
+            if(ModelState.IsValid)
+            {
+                _db.Add(productTypes);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+
+
+            }
+            return View();
+        }
+    }
+}
---
+++ GraniteOnlineStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GraniteOnlineStore.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GraniteOnlineStore.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+    }
+}
